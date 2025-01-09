e7b6ef4f10d1401af8d9fe00fff1261f62d91ebc
Nishant Sevak <nishantsevak7089@gmail.com>
2025-01-09 22:35:04 +0530

    cities repository added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 const express = require('express');
-const app = express();
+
 const {PORT} = require('./config/serverConfig');
 
 async function serverStart () {
+
+    const app = express();
+    app.use(express.json());
+    app.use(express.urlencoded({extended:true}))
    
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
+      
     });
 }
 
